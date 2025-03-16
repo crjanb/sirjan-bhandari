2e819f8f769c41db3f4c51e4de7fc410d8e8baea
crjanb <crjanb@gmail.com>
2025-03-16 11:47:46 +0545

    Add GitHub Actions workflow for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default {
   plugins: [react()],
-  base: '/sirjan-bhandari', // Replace 'sirjan-bhandari' with your repository name
+  base: '/', // Replace 'sirjan-bhandari' with your repository name
   build: {
     outDir: 'dist',
   },
